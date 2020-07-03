156e6e817d5e32408f479004deb8aa544bf803c0
avgre <avtar.g10@gmail.com>
2020-07-03 09:56:39 -0400

    setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        manualInit: true,
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
