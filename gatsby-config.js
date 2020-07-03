e7a89b4dd0cbf92f1d43e381f5e78840d7077dc9
avgre <avtar.g10@gmail.com>
2020-07-03 10:31:11 -0400

    +++

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
-        enableIdentityWidget: false,
+        enableIdentityWidget: true,
       },
     },
     {
